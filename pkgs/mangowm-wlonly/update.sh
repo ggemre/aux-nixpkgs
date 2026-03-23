635fc3498d6562f0a85b7e180dce7aacacce8992
ggemre <ggemre+github@proton.me>
2026-03-23 16:46:01 -0400

    fix: script deps

--- pkgs/mangowm-wlonly/update.sh
+++ pkgs/mangowm-wlonly/update.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 set -euo pipefail
-nix-update mangowm-wlonly --flake --version=branch=wl-only
-sed -i 's/version = "[A-Za-z0-9.-]*";/version = "wl-only-git";/' pkgs/mangowm-wlonly/default.nix 
+nix run nixpkgs#nix-update -- mangowm-wlonly --flake --version=branch=wl-only
+nix shell nixpkgs#gnused -c sed -i 's/version = "[A-Za-z0-9.-]*";/version = "wl-only-git";/' pkgs/mangowm-wlonly/default.nix 
