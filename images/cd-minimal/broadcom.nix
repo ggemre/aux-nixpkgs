468865a292c27f54bd6cf8cf2a1ffe0efcd2ecfb
Gage Moore <ggemre+github@proton.me>
2026-04-06 18:38:17 -0400

    fix: paths filter (#38)
    * fix: paths filter
    
    * nit: ordering

--- images/cd-minimal/broadcom.nix
+++ images/cd-minimal/broadcom.nix
@@ -1,7 +1,7 @@
 # Some of my hardware requires broadcom drivers which are not included by default
 {config, ...}: {
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [config.boot.kernelPackages.broadcom_sta.name];
   boot.kernelModules = ["wl"];
   boot.extraModulePackages = [config.boot.kernelPackages.broadcom_sta];
-  nixpkgs.config.permittedInsecurePackages = [config.boot.kernelPackages.broadcom_sta.name];
 }
