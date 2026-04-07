dba83da08b5e5e4159f6fd95b8f84cc32e9fb0d3
ggemre <ggemre+github@proton.me>
2026-04-07 11:31:19 -0400

    feat: compress images

--- images/cd-minimal/default.nix
+++ images/cd-minimal/default.nix
@@ -1,12 +1,9 @@
-{
-  modulesPath,
-  ...
-}: {
+{modulesPath, ...}: {
   imports = [
     (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
     ./broadcom.nix
   ];
 
-  networking.networkmanager.enable = true;
   nixpkgs.hostPlatform = "x86_64-linux";
+  isoImage.compressImage = true;
 }
