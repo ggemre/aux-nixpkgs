bbac9993ec071c19825a31d7c11bb3630ed575e8
ggemre <80432807+ggemre@users.noreply.github.com>
2026-04-10 14:02:12 -0400

    Revert "chore: revert mango version (#57)"
    This reverts commit 70cfe3d13777d7f6aca433c847da2e6802794107.

--- pkgs/mangowm-wlonly/update.sh
+++ pkgs/mangowm-wlonly/update.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 set -euo pipefail
 nix run nixpkgs#nix-update -- mangowm-wlonly --flake --version=branch=wl-only
+nix shell nixpkgs#gnused -c sed -i 's/version = "[A-Za-z0-9.-]*";/version = "wl-only-git";/' pkgs/mangowm-wlonly/default.nix 
